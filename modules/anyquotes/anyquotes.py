a99dee58665ed39e72635cca64559a6673bc578c
Luraminaki <gaetan.bayon-de-noyer@cpe.fr>
2025-05-02 20:08:19 +0200

    refactor: Check details - Changed module name - Input file is CSV and MUST have at least 'author' and 'quote' fields -  Quote templating in config file

---
+++ modules/anyquotes/anyquotes.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 21 13:47:01 2025
+
+@author: Luraminaki
+"""
+
+import csv
+import time
+import mmap
+import inspect
+import pathlib
+import random
+import linecache
+import logging
+import unidecode
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+random.seed(int(time.time()))
+
+CWD = pathlib.Path.cwd()
+
+
+class AnyQuotes:
+    def __init__(self, module_name: str=None, modules_config: dict=None) -> None:
+        if (not modules_config
+            or not module_name
+            or modules_config is None
+            or module_name is None):
+            raise ValueError(f"{self.__class__.__name__} -- Invalid module_name configuration file provided -- {module_name} : {modules_config}")
+
+        self.module_name: str = module_name
+        self.module_datas: dict[str, str] = modules_config['modules'][self.module_name]['datas']
+
+        data_path: pathlib.Path = pathlib.Path(modules_config['directories']['data_directory'])/self.module_name
+        input_files: list[pathlib.Path] = data_path.glob('*.csv', case_sensitive=False)
+        self.q_datas: dict[pathlib.Path,
+                           dict[str,
+                                int | str | list[str]]] = {file_name: {}
+                                                           for file_name in input_files}
+
+        for q_file in self.q_datas:
+            nbr_lines: int = 0
+
+            with q_file.open('r+') as qf:
+                reader = csv.reader(qf, delimiter=';')
+                header = next(reader)
+
+                buf = mmap.mmap(qf.fileno(), 0)
+
+                while buf.readline():
+                    nbr_lines += 1
+
+            self.q_datas[q_file] = {'nbr_lines': nbr_lines,
+                                    'header': header}
+
+
+    def pretty_quote(self, source_file: str, data_quote: dict[str, str]) -> str:
+        template: str = self.module_datas.get('template', '')
+        return (template.replace('<quote>', '\n- '.join(unidecode.unidecode(data_quote['quote']).split(' - ')))
+                        .replace('<author>', unidecode.unidecode(data_quote['author']))
+                        .replace('<source_file>', unidecode.unidecode(source_file)))
+
+
+    def get_random_quote_from_csv(self) -> list[str]:
+        curr_func = inspect.currentframe().f_code.co_name
+
+        if not self.q_datas:
+            logger.warning(f"{curr_func} -- Quote file folder is either empty or failed to be loaded")
+            return []
+
+        q_file, q_details = random.choice(list(self.q_datas.items()))
+        q_line: str = linecache.getline(str(q_file.absolute()),
+                                        random.randint(1, q_details['nbr_lines'])).rstrip('\r\n')
+
+        q_data: dict[str, str] = {elem_type.lower(): elem
+                                  for elem_type, elem in zip(q_details['header'],
+                                                             q_line.split(';'))
+                                  if isinstance(elem_type, str) and isinstance(elem, str)}
+
+        return self.pretty_quote(q_file.stem,
+                                 data_quote=q_data)
---
+++ modules/anyquotes/api_views.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 21 13:47:01 2025
+
+@author: Luraminaki
+"""
+
+import inspect
+import logging
+
+from modules.helpers import statics
+from modules.helpers import generic_api_views
+from modules.anyquotes import anyquotes
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+class QuotesView(generic_api_views.GenericViews):
+    def __init__(self, modules_config: dict=None, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.aq = anyquotes.AnyQuotes(self.module_name, modules_config)
+        self.api_router.add_api_route("/quote",
+                                      self.get_quote,
+                                      methods=['GET'],
+                                      description=f"Picks a random quote from the {', '.join(file.stem for file in self.aq.q_datas)} file(s) available")
+
+
+    async def get_quote(self) -> dict[str, str]:
+        curr_func = inspect.currentframe().f_code.co_name
+
+        try:
+            if not(res := self.aq.get_random_quote_from_csv()):
+                return {'status': statics.StatusFunction.ERROR.name, 'error': ''}
+
+        except Exception as err:
+            logger.error(f"{curr_func} -- {repr(err)}")
+            return {'status': statics.StatusFunction.ERROR.name, 'error': repr(err)}
+
+        return {'status': statics.StatusFunction.SUCCESS.name, 'data': res, 'error': ''}
